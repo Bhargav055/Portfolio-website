6c93a9886676fce408e5d7c727652fd0ed404545
Bhargav055 <150264062+Bhargav055@users.noreply.github.com>
2023-11-14 22:34:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+        if($(window).scrollTop()>0){
+            $('.top').show();
+        }
+        else{
+            $('.top').hide();
+        }
+    });
+    $(window).om('scroll load',function(){
+        $('#menu').removeclass('fa-times');
+        $('header').removeclass('fa-times');
+    })
+    $('a[href*="#]').on('click',function(e){
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:$($(this).attr('href')).offset().top,
+        },
+        500,
+        'linear',
+        );
+
+    });
+});
